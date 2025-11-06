7df610553076f8d1e2e6aeedba466db3d1957af3
Grigor Yeghiazaryan <yeghiazaryan@gmail.com>
2025-11-05 21:35:54 -0800

    Initial commit - Hello World PWA

---
+++ app.js
@@ -0,0 +1,12 @@
+// This file will handle service worker registration in the future
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful');
+            })
+            .catch(err => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
